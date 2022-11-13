060310884c2070a626d4f6d1df62c23c2b58607c
lgolikova <milagoul@gmail.com>
2022-11-13 21:58:51 +0300

    create app

---
+++ src/components/list.jsx
@@ -0,0 +1,72 @@
+const list = [{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+},
+{
+    id: "1",
+    en: "apple",
+    ru: "яблоко",
+    transcription: "",
+    tag: "фрукты"
+}];
+
+export default list;
