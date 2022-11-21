4fed5c28baff8d43361e80bde36b44a7707e9ccd
lgolikova <milagoul@gmail.com>
2022-11-21 20:43:28 +0300

    header styles

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from './components/Header/header';
 import Main from './components/Main/main';
 import Footer from './components/Footer/footer';
 import Card from './components/Card/card';
-import Table from './components/table';
+import Table from './components/Table/table';
 import List from './components/list';
+import '../../cards/src/assets/styles/variables.scss'
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import "./header.scss";
 
 function Header() {
-    return (<div></div>);
+    return (
+        <div className='header'>
+            <div className='header_logo'>
+                <img className='header_img' src='studying2.png' alt='logo' />
+                <div className='header_appname'>Learning English words</div>
+            </div>
+            <div className='header_nav'>
+                <a href='#'>Wordlist</a>
+                <a href='#'>Wordcards</a>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
