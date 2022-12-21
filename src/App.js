42988b4764e3f2423c929cfa94a6796aca4ef6bc
lgolikova <milagoul@gmail.com>
2022-12-21 22:18:26 +0300

    word card

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Header />
       <Table />
+      <Card />
     </div>
   );
 }
--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import list from '../list';
+import "./card.scss";
 
-function Card() {
-    return (<div></div>);
+function Card(props) {
+
+    const [isCheckMode, setIsCheckMode] = useState(false);
+
+    const handleCheck = () => {
+        setIsCheckMode(!isCheckMode);
+    }
+
+    return (
+        <div className='card-container'>
+            <div className='card'>
+                <div className='card-name'>{list[0].en}</div>
+                <div className="card-transcription">{list[0].transcription}</div>
+                {isCheckMode ? <div className='card-translation'>{list[0].ru}</div> : <button onClick={handleCheck} className='card-check_btn'>Check</button>}
+            </div>
+        </div>
+    );
 }
 
 export default Card;
